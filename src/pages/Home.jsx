850004a681ca38a09b887313a1b3c0fe599f2a64
Rohan.27s <rohan.gotwal@graphia.in>
2022-11-16 10:15:49 +0530

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div><h1>THE RUN</h1><button>PLAY</button><h2>PRESS ENTER TO PLAY</h2></div>
   )
 }
 
