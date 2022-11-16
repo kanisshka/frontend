595f52a63a6ca89eaca02a7599de261000b5660d
kanisshka <kanisshka12@gmail.com>
2022-11-16 18:42:33 +0530

    navbar

---
+++ src/components/Powerup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Powerup = () => {
+  return (
+    <div>Powerup</div>
+  )
+}
+
+export default Powerup
--- src/pages/MarketPlace.jsx
+++ src/pages/MarketPlace.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './MarketPlace.css';
+import Topbar from '../components/Topbar';
+import Powerup from '../components/Powerup';
 const MarketPlace = () => {
 
   return (
     <>
+      <div className="head">
+                    <Topbar />
+                </div>
       <div className='nav'>
         <ul>
-          <li class="active"><a href="">Power-Up</a></li>
+          <li class="active"><a>Power-Up</a></li>
           <li><a href="">Sprite</a></li>
           <li><a  href="">Characteristics</a></li>
         </ul>
