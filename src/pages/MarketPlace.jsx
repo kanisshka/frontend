bafb762db388b17eb905cc2da2e7dc5ddda0d020
kanisshka <kanisshka12@gmail.com>
2022-11-16 18:34:22 +0530

    navbar

--- src/pages/MarketPlace.jsx
+++ src/pages/MarketPlace.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import './MarketPlace.css';
 const MarketPlace = () => {
+
   return (
-    <div>MarketPlace</div>
+    <>
+      <div className='nav'>
+        <ul>
+          <li class="active"><a href="">Power-Up</a></li>
+          <li><a href="">Sprite</a></li>
+          <li><a  href="">Characteristics</a></li>
+        </ul>
+      </div>
+    </>
   )
 }
 
