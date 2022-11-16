2b6125ea12db12e54a9611ff6644f87968f7b004
Rohan.27s <rohan.gotwal@graphia.in>
2022-11-16 11:13:24 +0530

    homepage added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Home.css';
-
+import dino from '../assets/dino.png';
 const Home = () => {
   return (
-    <div><h1>THE RUN</h1><button>PLAY</button><h2>PRESS ENTER TO PLAY</h2></div>
+    <>
+    <div className='main'>
+      <h1>THE RUN</h1>
+      <img src={dino} alt="" />
+      <button>PLAY</button>
+      <h2 className='blink-soft'>PRESS ENTER TO PLAY</h2>
+      </div>
+      </>
   )
 }
 
