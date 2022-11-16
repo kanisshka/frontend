c985806286827fdc007b86eb2280f7eab2477d35
Rohan.27s <rohan.gotwal@graphia.in>
2022-11-16 15:51:27 +0530

    back button added

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Topbar.css'
 import user from '../assets/user.png'
+import {useNavigate} from 'react-router-dom';
+
 const Topbar = () => {
+    const navigate = useNavigate();
     return (
-        <>
+        <>        
             <div className="topbar">
-                <div className="coins"><h2>Coins:123</h2></div>
+                <button className='back' onClick={() => navigate(-1)}>BACK</button>
+                <div className="coins"><h2>Coins:{123}</h2></div>
                 <div className="user">
                     <img src={user} alt="" />
                 </div>
