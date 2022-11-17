60a6c7aeb57c89dc03d35f8adfc731aab0f0caa4
kanisshka <kanisshka12@gmail.com>
2022-11-17 19:14:49 +0530

    hey

--- src/components/Powerup.jsx
+++ src/components/Powerup.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Marketcard from './Marketcard'
-
+import Modal from './Modal';
 const Powerup = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
-   <> 
-   <Marketcard title={"2X SPEED"}/>
-    <Marketcard title={"2X HEALTH"}/>
+    <>
+    <a className='modalclass'
+      onClick={() => {
+        setModalOpen(true);
+        console.log("true");
+      }} >
+      <Marketcard
+      title={"2X SPEED"} /> 
+      </a>
+      <Marketcard title={"2X HEALTH"} />
+      {modalOpen && <Modal setOpenModal={setModalOpen} />}
     </>
   )
 }
