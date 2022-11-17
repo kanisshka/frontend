c90f23e38b3c0ddf91fbb066fcedfb33c8569bcd
Rohan.27s <rohan.gotwal@graphia.in>
2022-11-17 09:43:44 +0530

    navbar working on marketplace

---
+++ src/components/Sprite.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sprite = () => {
+  return (
+    <div>Sprite</div>
+  )
+}
+
+export default Sprite
--- src/pages/MarketPlace.jsx
+++ src/pages/MarketPlace.jsx
@@ -2,22 +2,36 @@ import React, { useState } from 'react'
 import './MarketPlace.css';
 import Topbar from '../components/Topbar';
 import { Link } from 'react-router-dom';
+import Powerup from '../components/Powerup';
+import Sprite from '../components/Sprite';
+import Characteristics from '../components/Characteristics';
 const MarketPlace = () => {
-const [present,setPresent] = useState("powerup");
+  const [present, setPresent] = useState("powerup");
+  let currentCompo;
+  if (present == "powerup") {
+    currentCompo = <Powerup />;
+  }
+  else if (present == "sprite") {
+    currentCompo = <Sprite />;
+  }
+  else {
+    currentCompo =<Characteristics />
+  }
+
   return (
     <>
       <div className="head">
-                    <Topbar />
-                </div>
+        <Topbar />
+      </div>
       <div className='nav'>
         <ul>
-        <li class="active"><a onClick={()=>setPresent("powerup")}>Power-Up</a></li> 
-          <li><a onClick={()=>setPresent("sprite")} href="">Sprite</a></li>
-          <li><a onClick={()=>setPresent("charactertistics")} href="">Characteristics</a></li>
+          <li class="active"><a onClick={() => setPresent("powerup")}>Power-Up</a></li>
+          <li><a onClick={() => setPresent("sprite")} href="">Sprite</a></li>
+          <li><a onClick={() => setPresent("charactertistics")} href="">Characteristics</a></li>
         </ul>
       </div>
       <div className='compont'>
-         
+        {currentCompo}
       </div>
     </>
   )
