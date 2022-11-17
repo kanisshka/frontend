cf70aac958e7ab95d820d5fdf02f33c2e7cc5c53
kanisshka <kanisshka12@gmail.com>
2022-11-17 09:29:26 +0530

    dropdown

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import './Topbar.css'
 import user from '../assets/user.png'
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Topbar = () => {
     const navigate = useNavigate();
     return (
-        <>        
+        <>
             <div className="topbar">
                 <button className='back' onClick={() => navigate(-1)}>BACK</button>
                 <div className="coins"><h2>Coins:{123}</h2></div>
                 <div className="user">
                     <img src={user} alt="" />
-                </div>
-                
+                    <ul className="dropdown-content">
+                        <li>Profile</li>
+                        <li>Logout</li>
+
+                    </ul>
             </div>
+
+        </div>
         </>
     )
 }
