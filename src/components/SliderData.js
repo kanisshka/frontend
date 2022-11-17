7a09bd131fa5a13b5a0a6e039af64a1e6a4d40fd
Rohan.27s <rohan.gotwal@graphia.in>
2022-11-17 14:49:39 +0530

    jaehl

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -5,7 +5,7 @@ export const SliderData = [
     health:'40',
     jump:'20',
     image:
-      'https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+      '../assets/dino.png'
   },
   {
     name:'dino2',
@@ -13,7 +13,7 @@ export const SliderData = [
     health:'40',
     jump:'20',
     image:
-      'https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80'
+    '../assets/dino.png'
   },
   {
     name:'dino3',
@@ -21,7 +21,7 @@ export const SliderData = [
     health:'47',
     jump:'20',
     image:
-      'https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+    '../assets/dino.png'
   },
   {
     name:'dino4',
@@ -29,7 +29,7 @@ export const SliderData = [
     health:'49',
     jump:'20',
     image:
-      'https://images.unsplash.com/photo-1475189778702-5ec9941484ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80'
+    '../assets/dino.png'
   },
   {
     name:'dino5',
@@ -37,6 +37,6 @@ export const SliderData = [
     health:'49',
     jump:'20',
     image:
-      'https://images.unsplash.com/photo-1503177119275-0aa32b3a9368?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+    '../assets/dino.png'
   }
 ];
